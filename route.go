296d3b3570205065def440c719ae240ab7e2faaf
Kevin Viricel <kevin.viricel@gmail.com>
2015-04-21 14:09:02 +0300

    Add basic routing

--- route.go
+++ route.go
@@ -1,15 +1,57 @@
-package groot
+package router
 
 import (
+	"fmt"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 type Route struct {
-	// Specify the HTTP method (GET, POST, PUt, etc...)
-	httpMethod string
-	// Regexp to match route
+	method     string
 	pathRegexp *regexp.Regexp
-	// Route request handler
-	handler http.Handler
+	handler    http.Handler
+}
+
+// Handler sets a handler for the route.
+func (this *Route) Handler(handler http.Handler) *Route {
+	this.handler = handler
+	return this
+}
+
+// HandlerFunc sets a handler function for the route.
+func (this *Route) HandlerFunc(f func(http.ResponseWriter, *http.Request)) *Route {
+	return this.Handler(http.HandlerFunc(f))
+}
+
+func (this *Route) PathPrefix(template string) *Route {
+	r := regexp.MustCompile(`{[^/#?()\.\\]+}`)
+
+	tmpRegx := r.ReplaceAllStringFunc(template, func(m string) string {
+		m = m[1 : len(m)-1]
+		return fmt.Sprintf(`(?P<%s>[^/#?]+)`, m)
+	})
+
+	this.pathRegexp = regexp.MustCompile("^" + tmpRegx + ".*$")
+	return this
+}
+
+// Path defines URL template for the route.
+// It accepts a template with zero or more URL variables enclosed by {}.
+func (this *Route) Path(template string) *Route {
+	r := regexp.MustCompile(`{[^/#?()\.\\]+}`)
+
+	tmpRegx := r.ReplaceAllStringFunc(template, func(m string) string {
+		m = m[1 : len(m)-1]
+		return fmt.Sprintf(`(?P<%s>[^/#?]+)`, m)
+	})
+
+	this.pathRegexp = regexp.MustCompile("^" + tmpRegx + "$")
+	return this
+}
+
+// Method sets a method for the route.
+func (this *Route) Method(method string) *Route {
+	this.method = strings.ToUpper(method)
+	return this
 }
--- router.go
+++ router.go
@@ -1,6 +1,7 @@
-package groot
+package router
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -11,4 +12,80 @@ type Router struct {
 	PanicHandler http.Handler
 	// Routes to be matched, in order.
 	routes []*Route
+}
+
+// If a panic handler was defined it calls
+func (this *Router) handlePanic(w http.ResponseWriter, r *http.Request) {
+	if this.PanicHandler != nil {
+		err := recover()
+		if err != nil {
+			log.Println(err)
+			this.PanicHandler.ServeHTTP(w, r)
+		}
+	}
+}
+
+func (this *Router) parseRequestParams(r *http.Request, route *Route, matches []string) {
+	r.ParseForm()
+	if len(matches) > 0 && matches[0] == r.URL.Path {
+		for i, name := range route.pathRegexp.SubexpNames() {
+			if len(name) > 0 {
+				r.Form.Add(name, matches[i])
+			}
+		}
+	}
+}
+
+// NewRouter returns a new router instance.
+func NewRouter() *Router {
+	return &Router{}
+}
+
+// NewRoute registers an empty route.
+func (this *Router) NewRoute() *Route {
+	r := &Route{}
+	r.method = "GET"
+	this.routes = append(this.routes, r)
+	return r
+}
+
+func (this *Router) Handle(path string, handler http.Handler) *Route {
+	return this.NewRoute().Path(path).Handler(handler)
+}
+
+func (this *Router) HandleFunc(path string, f func(http.ResponseWriter,
+	*http.Request)) *Route {
+	return this.NewRoute().Path(path).HandlerFunc(f)
+}
+
+func (this *Router) FindMatchingRoute(r *http.Request) *Route {
+	for _, route := range this.routes {
+		if route.pathRegexp != nil && route.method == r.Method {
+			if matches := route.pathRegexp.FindStringSubmatch(r.URL.Path); matches != nil {
+				this.parseRequestParams(r, route, matches)
+				return route
+			}
+		}
+	}
+	return nil
+}
+
+func (this *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer this.handlePanic(w, r)
+
+	var handler http.Handler
+	var matchingRoute *Route
+
+	if matchingRoute = this.FindMatchingRoute(r); matchingRoute != nil {
+		handler = matchingRoute.handler
+	}
+
+	if handler == nil {
+		handler = this.NotFoundHandler
+		if handler == nil {
+			handler = http.NotFoundHandler()
+		}
+	}
+
+	handler.ServeHTTP(w, r)
 }
